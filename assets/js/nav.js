ef9b35bd9c1afca1c21159b40328826f01c4a23c
v-mochalov <deathguuun@gmail.com>
2022-05-30 18:15:08 +0300

    header change

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
 		$('.header__burger, .header__menu').removeClass('active');
 		$('body').removeClass('lock')
 	});
+
+	
 	//Adaptive SlickSlider 
 	$(window).on('resize', function (e) {
 
@@ -36,43 +38,6 @@ $(document).ready(function () {
 		}
 	}).trigger('resize');
 })
-
-
-// //TIMER
-
-// var deadline = '06 04 2022 12:00:00 GMT+0300';
-// function time_remaining(endtime) {
-// 	var t = Date.parse(endtime) - Date.parse(new Date());
-// 	var seconds = Math.floor((t / 1000) % 60);
-// 	var minutes = Math.floor((t / 1000 / 60) % 60);
-// 	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-// 	var days = Math.floor(t / (1000 * 60 * 60 * 24));
-// 	return { 'total': t, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds };
-// }
-// function run_clock(id, endtime) {
-// 	var clock = document.getElementById(id);
-
-
-// 	var days_span = clock.querySelector('.days');
-// 	var hours_span = clock.querySelector('.hours');
-// 	var minutes_span = clock.querySelector('.minutes');
-// 	var seconds_span = clock.querySelector('.seconds');
-
-// 	function update_clock() {
-// 		var t = time_remaining(endtime);
-
-
-// 		days_span.innerHTML = t.days;
-// 		hours_span.innerHTML = ('0' + t.hours).slice(-2);
-// 		minutes_span.innerHTML = ('0' + t.minutes).slice(-2);
-// 		seconds_span.innerHTML = ('0' + t.seconds).slice(-2);
-
-// 		if (t.total <= 0) { clearInterval(timeinterval); }
-// 	}
-// 	update_clock();
-// 	var timeinterval = setInterval(update_clock, 1000);
-// }
-// run_clock('clockdiv', deadline);
 
 
 //slider
