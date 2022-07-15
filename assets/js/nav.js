30db9afcc0199427dd93ab107b09d70b3b0d189e
v-mochalov <deathguuun@gmail.com>
2022-07-15 15:17:31 +0300

    change acordeon

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -154,8 +154,8 @@ $(document).ready(function () {
 	
 		$('.block__title').click(function (acordeon) {
 			if ($('.block').hasClass('one')) {
-				$('.block__title').not($(this)).removeClass('active');
-				$('.block__text').not($(this).next()).slideUp(300);
+				// $('.block__title').not($(this)).removeClass('active');
+				// $('.block__text').not($(this).next()).slideUp(300);
 			}
 			$(this).toggleClass('active').next().slideToggle(300);
 		});
